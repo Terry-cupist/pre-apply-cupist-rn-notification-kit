5066470e1b7f2ffb6832039feac89a488d9a17ce
Terry-cupist <terry@cupist.com>
2025-10-28 14:44:57 +0900

    chore: context value 타입 확장시 partial 처리

--- src/app/hooks/types.ts
+++ src/app/hooks/types.ts
@@ -2,7 +2,7 @@ import { NotificationManageContextValue } from "@cupist/notification-core";
 import { DependencyList } from "react";
 
 export interface UseRNHookBaseProps<T extends (...args: any) => any>
-  extends NotificationManageContextValue {
+  extends Partial<NotificationManageContextValue> {
   onClickResponse?: (params: ReturnType<T>) => void;
   getValidNotificationData?: (params: ReturnType<T>) => ReturnType<T>;
   dependencies?: DependencyList;
