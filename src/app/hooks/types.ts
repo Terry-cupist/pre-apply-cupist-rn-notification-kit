12fab201f3df7910284e92b280c2930ef77304ac
Terry-cupist <terry@cupist.com>
2025-10-28 14:37:50 +0900

    feat: RN 백그라운드 클릭 응답 처리 개선
    - notification core 컨텍스트 연동으로 사용자 이벤트 전송
    
    - 딥링크 검증과 네비게이션을 훅에서 공통 처리

--- src/app/hooks/types.ts
+++ src/app/hooks/types.ts
@@ -1,6 +1,9 @@
+import { NotificationManageContextValue } from "@cupist/notification-core";
 import { DependencyList } from "react";
 
-export interface UseRNHookBaseProps<T extends (...args: any) => any> {
-  onClickResponse: (params: ReturnType<T>) => void;
+export interface UseRNHookBaseProps<T extends (...args: any) => any>
+  extends NotificationManageContextValue {
+  onClickResponse?: (params: ReturnType<T>) => void;
+  getValidNotificationData?: (params: ReturnType<T>) => ReturnType<T>;
   dependencies?: DependencyList;
 }
--- src/app/hooks/useRNBackgroundClickResponseListener.ts
+++ src/app/hooks/useRNBackgroundClickResponseListener.ts
@@ -1,17 +1,39 @@
-import { parseReactNativeNotification } from "@cupist/notification-core";
+import {
+  parseReactNativeNotification,
+  useNotificationManage,
+} from "@cupist/notification-core";
 import { RNNotificationModule } from "@shared/notification";
 import { useEffect } from "react";
 import { UseRNHookBaseProps } from "./types";
 
-export const useRNBackgroundClickResponseListener = ({
-  onClickResponse,
-  dependencies = [],
-}: UseRNHookBaseProps<typeof parseReactNativeNotification>) => {
+export const useRNBackgroundClickResponseListener = (
+  props?: UseRNHookBaseProps<typeof parseReactNativeNotification>,
+) => {
+  const {
+    onClickResponse,
+    getValidNotificationData,
+    dependencies = [],
+  } = props ?? {};
+  const { sendNotificationUserEvent, refreshDeepLinkApis, navigateToLink } =
+    useNotificationManage(props);
   useEffect(() => {
     RNNotificationModule.configure({
       onNotification: (notification) => {
         const parsedNotification = parseReactNativeNotification(notification);
-        onClickResponse(parsedNotification);
+        const validNotificationData = getValidNotificationData
+          ? getValidNotificationData?.(parsedNotification)
+          : parsedNotification;
+
+        if (validNotificationData.type) {
+          sendNotificationUserEvent(validNotificationData.type);
+        }
+
+        if (validNotificationData.deepLink) {
+          refreshDeepLinkApis(validNotificationData.deepLink);
+          navigateToLink(validNotificationData.deepLink);
+        }
+
+        onClickResponse?.(parsedNotification);
       },
     });
   }, dependencies);
